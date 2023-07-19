a17b348d7e034d0255c85e8c929160f8e8e54e36
Romero <rrcb@cin.ufpe.br>
2023-07-19 11:57:02 -0300

    Create signup frontend
    Needs to integrate frontend with backend

--- bookverse/gui/controllers_frontend.py
+++ bookverse/gui/controllers_frontend.py
@@ -1,13 +1,30 @@
-from flask import render_template
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+
+from ..database import db_session
+
 app = Flask(__name__)
 
-from bookverse.database import db_session
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
+
 @app.route('/login')
-def test():
+def login():
     return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        full_name = request.form['full_name']
+        email = request.form['email']
+        password = request.form['password']
+        password2 = request.form['password2']
+
+        return render_template('signup.html', success="success")
+    return render_template('signup.html')
