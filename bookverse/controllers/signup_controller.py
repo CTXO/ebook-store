cef9a24463c0c67dbc0d5bcdcc75e68df39dfcd4
Romero <rrcb@cin.ufpe.br>
2023-07-21 11:50:32 -0300

    Improve signup
    Create card and library associated with the new user

--- bookverse/controllers/signup_controller.py
+++ bookverse/controllers/signup_controller.py
@@ -1,4 +1,7 @@
 import random
+
+from ..cart.cart_crud import CartCrud
+from ..library.library_crud import LibraryCrud
 from ..user.user_crud import UserCrud
 from werkzeug.security import generate_password_hash
 
@@ -6,6 +9,8 @@ from werkzeug.security import generate_password_hash
 class SignupController:
     def __init__(self):
         self.user_crud = UserCrud()  # TODO inject this dependency
+        self.cart_crud = CartCrud()
+        self.library_crud = LibraryCrud()
 
     def signup(self, full_name, email, password, password2):
         existing_email = self.user_crud.retrieve_by_email(email)
@@ -20,6 +25,9 @@ class SignupController:
 
         password_hash = generate_password_hash(password)
         new_user = self.user_crud.create(full_name, email, password_hash)
+        self.cart_crud.create_cart(new_user.id)
+        self.library_crud.create_library(new_user.id)
+
         return {"success": True, "message": None, "user": new_user}
 
 
