fbfb32b9e8ac547f21a85b1dfae0314128a1b5f2
Romero <rrcb@cin.ufpe.br>
2023-07-21 09:43:01 -0300

    Change user table name

--- bookverse/cart/models.py
+++ bookverse/cart/models.py
@@ -3,7 +3,7 @@ from ..app import db
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     ebooks = db.relationship('Ebook', backref='cart')
 
     def __init__(self, user_id):
--- bookverse/user/models.py
+++ bookverse/user/models.py
@@ -2,7 +2,7 @@ from ..app import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(50), nullable=False)
