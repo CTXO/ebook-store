b1d1c4c49f754d847650044c50734eea7b23bc57
Romero <rrcb@cin.ufpe.br>
2023-07-21 09:33:43 -0300

    Create cart model

---
+++ bookverse/cart/models.py
@@ -0,0 +1,12 @@
+from ..app import db
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    ebooks = db.relationship('Ebook', backref='cart')
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+
